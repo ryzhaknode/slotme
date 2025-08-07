18394f7e1827d082004cef65eb91bdb194e27760
MykhailoVobolis <mvobolis@gmail.com>
2025-08-07 13:35:54 +0300

    initial project

---
+++ client/src/components/App/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Real Time Chat</h1>
+    </div>
+  );
+}
